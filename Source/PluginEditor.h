721d972ebdf47e473cc69a939c27ddd7813181c3
Jrsnovo23 <ccmbaudio@gmail.com>
2026-09-19 01:33:57 -0600

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <juce_audio_processors/juce_audio_processors.h>
 #include "PluginProcessor.h"
-#include "../UI/PPGLookAndFeel.h"
+#include "UI/PPGLookAndFeel.h"
 
 class PPGWave3Editor : public juce::AudioProcessorEditor
 {
